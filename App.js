c2d0117c0c88a3dbc5a76db100deed1e1662b6f9
Pedro Airton <111513226+pedroairton@users.noreply.github.com>
2023-06-14 02:25:58 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
 
 import { NavigationContainer } from '@react-navigation/native'
 import Routes from './src/routes'
+import { reducer } from './src/reducers/reducer';
+
+const store = createStore(reducer);
+
 
 export default function App() {
   return (
+    <Provider store={store}>
     <NavigationContainer>
       <StatusBar backgroundColor="#2F3C7E" barStyle="light-content"/>
       <Routes/>
     </NavigationContainer>
+    </Provider>
+    
   );
 }
+
+// export default () => {
+//   return (
+//     <Provider store={store}>
+//       <NavigationContainer>
+//         <App />
+//       </NavigationContainer>
+//     </Provider>
+//   );
+// };
