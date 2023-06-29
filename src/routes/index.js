cb07a5a4893200fdb4bb64a667f77158180230ea
Pedro Airton <111513226+pedroairton@users.noreply.github.com>
2023-06-29 20:51:19 -0300

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator, StackNavigationOptions } from '@react-navigation/native-stack'
+import { View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
 
 import Welcome from '../pages/Welcome'
 import SignIn from '../pages/SignIn'
@@ -7,16 +9,46 @@ import Form from '../pages/Form'
 import Results from '../pages/Results'
 import FormConfirmed from '../pages/FormConfirmed'
 import Cadastro from '../pages/Cadastro'
+import Cadastrar from '../pages/Cadastrar'
 import AdmBridge from '../pages/AdmBridge'
 import Profiles from '../pages/Profiles'
-import EditUser from '../pages/EditUser'
+import FormSelect from '../pages/FormSelect'
 
 
 const Stack = createNativeStackNavigator()
 
+
 export default function Routes(){
+    const navigation = useNavigation()
+
+    const handleHeaderPress = () => {
+        navigation.navigate('Welcome'); 
+      }
     return(
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+            headerStyle:{
+                backgroundColor: '#2f3c7e'
+            },
+            headerTintColor: 'white',
+            headerShadowVisible: false,
+            headerTitle: () => {
+                return (
+                <TouchableOpacity onPress={handleHeaderPress}>
+                    <Text style={{ 
+                        fontSize: 19,
+                        fontWeight: 'bold',
+                        textAlign: 'center',
+                        fontWeight:'bold',
+                        letterSpacing: 1,
+                        borderBottomWidth: 1,
+                        borderBottomColor: 'white',
+                        color: 'white'
+                        }}>FORMIFY</Text>
+                </TouchableOpacity>
+                )
+            },
+            headerTitleAlign:'center',
+        }}>
            <Stack.Screen
             name="Welcome"
             component={Welcome}
@@ -58,8 +90,12 @@ export default function Routes(){
             component={Profiles}
            />
            <Stack.Screen
-            name="EditUser"
-            component={EditUser}
+            name="FormSelect"
+            component={FormSelect}
+           />
+           <Stack.Screen
+            name="Cadastrar"
+            component={Cadastrar}
            />
            
         </Stack.Navigator>
